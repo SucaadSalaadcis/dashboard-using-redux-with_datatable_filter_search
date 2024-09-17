3d89fac9e494af3634cc68eb1636909218d0bccc
SucaadSalaadcis <sucaadsalaad520@gmail.com>
2024-09-17 16:09:02 +0300

    removed widgets

--- pages/tables/products/Category.jsx
+++ pages/tables/products/Category.jsx
@@ -245,15 +245,6 @@ export default function Category() {
                                     </ul>
                                 </li>
                                 <li class="nav-item">
-                                    <Link to={'/widgets'} class="nav-link">
-                                        <i class="nav-icon fas fa-th"></i>
-                                        <p>
-                                            Widgets
-                                            <span class="right badge badge-danger">New</span>
-                                        </p>
-                                    </Link>
-                                </li>
-                                <li class="nav-item">
                                     <a href="#" class="nav-link">
                                         <i class="nav-icon fas fa-copy"></i>
                                         <p>
