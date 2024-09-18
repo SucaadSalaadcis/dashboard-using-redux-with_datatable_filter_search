7d062c3bcffd726803b559c883a7a2f819db9329
SucaadSalaadcis <sucaadsalaad520@gmail.com>
2024-09-18 13:25:16 +0300

    some change

--- pages/layout/Boxed.jsx
+++ pages/layout/Boxed.jsx
@@ -183,7 +183,7 @@ export default function Boxed() {
                         <nav class="mt-2">
                             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                                 {/* <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library --> */}
+                                   with font-awesome or any other icon font library --> */}
                                 <li class="nav-item">
                                     <a href="#" class="nav-link">
                                         <i class="nav-icon fas fa-tachometer-alt"></i>
