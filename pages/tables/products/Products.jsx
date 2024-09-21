6fb5a83e000f071442310500901ecd55ffc09d5b
SucaadSalaadcis <sucaadsalaad520@gmail.com>
2024-09-21 12:46:48 +0300

    reusible nav, footer

--- pages/tables/products/Products.jsx
+++ pages/tables/products/Products.jsx
@@ -7,6 +7,7 @@ import Logout from '../../../pages/examples/Logout'
 import './Products.css';
 import data from './data'
 import FilterButtons from '../../tables/reusible/FilterButtons'
+import { BrandLogo, Footer, Navbar, SearchForm, UserPanel } from '../reusible/Sidebar';
 
 const Products = () => {
   const [searchQuery, setSearchQuery] = useState(''); // State for search query
@@ -36,10 +37,10 @@ const Products = () => {
 
   // all buttons
   const allButtons = [
-    { label: 'Tshirt', value: 'tshirt', count: 300, badgeClass: 'bg-success', iconClass: 'fas fa-tshirt', text: "text-primary"},
+    { label: 'Tshirt', value: 'tshirt', count: 300, badgeClass: 'bg-success', iconClass: 'fas fa-tshirt', text: "text-primary" },
     { label: 'Bag', value: 'bag', count: 891, badgeClass: 'bg-purple', iconClass: 'fas fa-shopping-bag', text: "text-warning" },
     { label: 'Dress', value: 'dress', count: 67, badgeClass: 'bg-teal', iconClass: 'fas fa-user', text: "text-pink" },
-    { label: 'Shoes', value: 'shoes', count: 12, badgeClass: 'bg-info', iconClass: 'fas fa-shoe-prints', text: 'text-green'},
+    { label: 'Shoes', value: 'shoes', count: 12, badgeClass: 'bg-info', iconClass: 'fas fa-shoe-prints', text: 'text-green' },
     { label: 'All', value: 'all', count: 542, badgeClass: 'bg-danger', iconClass: 'fas fa-heart', text: 'text-red' },
   ];
 
@@ -61,7 +62,8 @@ const Products = () => {
   const searchData = filteredItems.filter(item =>
     item.name.toLowerCase().includes(searchQuery.toLowerCase())
   )
-  console.log(searchQuery); // the search data
+  // console.log(searchQuery); // the search data
+  
   // Use searchData if searching, else use all products
   const displayData = searchQuery.length > 0 ? searchData : currentItem;
 
@@ -71,174 +73,20 @@ const Products = () => {
     <body class="hold-transition sidebar-mini">
       <div class="wrapper">
         {/* <!-- Navbar --> */}
-        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-          {/* <!-- Left navbar links --> */}
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-            </li>
-            <li class="nav-item d-none d-sm-inline-block">
-              <Link to={'/'} onClick={RefreshPage} class="nav-link">Home</Link>
-            </li>
-            <li class="nav-item d-none d-sm-inline-block">
-              <a href="#" class="nav-link">Contact</a>
-            </li>
-          </ul>
-
-          {/* <!-- Right navbar links --> */}
-          <ul class="navbar-nav ml-auto">
-            {/* <!-- Navbar Search --> */}
-            <li class="nav-item">
-              <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-                <i class="fas fa-search"></i>
-              </a>
-              <div class="navbar-search-block">
-                <form class="form-inline">
-                  <div class="input-group input-group-sm">
-                    <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search" />
-                    <div class="input-group-append">
-                      <button class="btn btn-navbar" type="submit">
-                        <i class="fas fa-search"></i>
-                      </button>
-                      <button class="btn btn-navbar" type="button" data-widget="navbar-search">
-                        <i class="fas fa-times"></i>
-                      </button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </li>
-
-            {/* <!-- Messages Dropdown Menu --> */}
-            <li class="nav-item dropdown">
-              <a class="nav-link" data-toggle="dropdown" href="#">
-                <i class="far fa-comments"></i>
-                <span class="badge badge-danger navbar-badge">3</span>
-              </a>
-              <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                <a href="#" class="dropdown-item">
-                  {/* <!-- Message Start --> */}
-                  <div class="media">
-                    <img src="../../dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle" />
-                    <div class="media-body">
-                      <h3 class="dropdown-item-title">
-                        Brad Diesel
-                        <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
-                      </h3>
-                      <p class="text-sm">Call me whenever you can...</p>
-                      <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                    </div>
-                  </div>
-                  {/* <!-- Message End --> */}
-                </a>
-                <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item">
-                  {/* <!-- Message Start --> */}
-                  <div class="media">
-                    <img src="../../dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3" />
-                    <div class="media-body">
-                      <h3 class="dropdown-item-title">
-                        John Pierce
-                        <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
-                      </h3>
-                      <p class="text-sm">I got your message bro</p>
-                      <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                    </div>
-                  </div>
-                  {/* <!-- Message End --> */}
-                </a>
-                <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item">
-                  {/* <!-- Message Start --> */}
-                  <div class="media">
-                    <img src="../../dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3" />
-                    <div class="media-body">
-                      <h3 class="dropdown-item-title">
-                        Nora Silvester
-                        <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
-                      </h3>
-                      <p class="text-sm">The subject goes here</p>
-                      <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                    </div>
-                  </div>
-                  {/* <!-- Message End --> */}
-                </a>
-                <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
-              </div>
-            </li>
-            {/* <!-- Notifications Dropdown Menu --> */}
-            <li class="nav-item dropdown">
-              <a class="nav-link" data-toggle="dropdown" href="#">
-                <i class="far fa-bell"></i>
-                <span class="badge badge-warning navbar-badge">15</span>
-              </a>
-              <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                <span class="dropdown-item dropdown-header">15 Notifications</span>
-                <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item">
-                  <i class="fas fa-envelope mr-2"></i> 4 new messages
-                  <span class="float-right text-muted text-sm">3 mins</span>
-                </a>
-                <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item">
-                  <i class="fas fa-users mr-2"></i> 8 friend requests
-                  <span class="float-right text-muted text-sm">12 hours</span>
-                </a>
-                <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item">
-                  <i class="fas fa-file mr-2"></i> 3 new reports
-                  <span class="float-right text-muted text-sm">2 days</span>
-                </a>
-                <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-              </div>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-                <i class="fas fa-expand-arrows-alt"></i>
-              </a>
-            </li>
-            {/* <li class="nav-item">
-            <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-                <i class="fas fa-th-large"></i>
-            </a>
-        </li> */}
-          </ul>
-        </nav>
+        <Navbar navImg1={'../../dist/img/user1-128x128.jpg'} navImg2={'../../dist/img/user8-128x128.jpg'} navImg3={'../../dist/img/user3-128x128.jpg'} />
         {/* <!-- /.navbar --> */}
 
         {/* <!-- Main Sidebar Container --> */}
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
           {/* <!-- Brand Logo --> */}
-          <Link to={'/'} onClick={RefreshPage} class="brand-link">
-            <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style={{ opacity: ".8" }} />
-            <span class="brand-text font-weight-light">ADWAAR</span>
-          </Link>
+          <BrandLogo />
 
-          {/* <!-- Sidebar --> */}
-          <div class="sidebar">
-            {/* <!-- Sidebar user (optional) --> */}
-            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-              <div class="image">
-                <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image" />
-              </div>
-              <div class="info">
-                <a href="#" class="d-block">Eng Bashiir</a>
-              </div>
-            </div>
+          <div className="sidebar">
+            {/* userPanel */}
+            <UserPanel />
 
-            {/* <!-- SidebarSearch Form --> */}
-            <div class="form-inline">
-              <div class="input-group" data-widget="sidebar-search">
-                <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search" />
-                <div class="input-group-append">
-                  <button class="btn btn-sidebar">
-                    <i class="fas fa-search fa-fw"></i>
-                  </button>
-                </div>
-              </div>
-            </div>
+            {/* sidebarSearch Form  */}
+            <SearchForm />
 
             {/* <!-- Sidebar Menu --> */}
             <nav class="mt-2">
@@ -505,9 +353,9 @@ const Products = () => {
                   ))
                 ) : (
                   <>
-               
-                    <p style={{color: 'red', fontSize: '25px'}}>No products found <BsEmojiFrown style={{fontSize: '30px', marginTop: 'px'}}/></p>
-                    
+
+                    <p style={{ color: 'red', fontSize: '25px' }}>No products found <BsEmojiFrown style={{ fontSize: '30px', marginTop: 'px' }} /></p>
+
                   </>
                 )}
 
@@ -537,18 +385,10 @@ const Products = () => {
           {/* <!-- /.content --> */}
         </div>
         {/* <!-- /.content-wrapper --> */}
-        <footer class="main-footer">
-          <div class="float-right d-none d-sm-block">
-            <b>Version</b> 3.2.0
-          </div>
-          <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-        </footer>
 
-        {/* <!-- Control Sidebar --> */}
-        <aside class="control-sidebar control-sidebar-dark">
-          {/* <!-- Control sidebar content goes here --> */}
-        </aside>
-        {/* <!-- /.control-sidebar --> */}
+        {/* footer */}
+        <Footer />
+
       </div>
       {/* <!-- ./wrapper --> */}
 
